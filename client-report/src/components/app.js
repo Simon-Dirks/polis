37aaefeae24f3f80afaa958731ebb14de9a3e5a0
Simon <Simon-Dirks@users.noreply.github.com>
2023-10-24 12:29:28 +0200

    strokeWidth instead of stroke-width

--- client-report/src/components/app.js
+++ client-report/src/components/app.js
@@ -486,7 +486,7 @@ class App extends React.Component {
                                 cy="12"
                                 r="10"
                                 stroke="currentColor"
-                                stroke-width="4"
+                                strokeWidth="4"
                             ></circle>
                             <path
                                 className="opacity-75"
