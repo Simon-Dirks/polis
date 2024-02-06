13d485b065f44209849fb8a7c598701c1b5f52ae
Simon <Simon-Dirks@users.noreply.github.com>
2024-02-06 12:19:18 +0100

    Allow hover

--- client-report/src/components/varianceChart/varianceChart.js
+++ client-report/src/components/varianceChart/varianceChart.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import * as d3 from 'd3'
 
+const circleColor = '#D9D9D9'
+const circleColorOnHover = '#929292'
 class VarianceChart extends Component {
     constructor(props) {
         super(props)
         this.state = {
             numRows: 4, // Number of rows
-            numCirclesPerRow: 15, // Number of circles per row
+            numCirclesPerRow: 36, // Number of circles per row
             padding: 5, // Fixed padding between circles
         }
         this.svgRef = React.createRef()
@@ -21,11 +23,11 @@ class VarianceChart extends Component {
         window.removeEventListener('resize', this.handleResize)
     }
 
-    handleResize = () => {
+    handleResize() {
         this.createSVG()
     }
 
-    calculateRadius = (width, numCirclesPerRow, padding) => {
+    calculateRadius(width, numCirclesPerRow, padding) {
         const totalPaddingWidth = (numCirclesPerRow - 1) * padding
         return (width - totalPaddingWidth) / (numCirclesPerRow * 2)
     }
@@ -56,6 +58,9 @@ class VarianceChart extends Component {
 
         const padding = this.state.padding - 1 // quick fix for making sure the very far right pixel of a circle is not cut off
 
+        const timeForIntroAnimationInMs = 750
+        const delayBetweenCirclesInMs = 4
+        let introAnimationCompleted = false
         svg.selectAll('circle')
             .data(circlesData)
             .enter()
@@ -63,13 +68,48 @@ class VarianceChart extends Component {
             .attr('cx', (d) => (d.index + 0.5) * (circleRadius * 2 + padding))
             .attr('cy', (d) => (d.row + 0.5) * (circleRadius * 2 + padding))
             .attr('r', circleRadius / 2)
-            .attr('fill', 'steelblue')
+            .attr('fill', '#D9D9D9')
             .attr('opacity', '0')
             .transition()
-            .duration(1000)
-            .delay((d, i) => i * 15)
+            .duration(timeForIntroAnimationInMs)
+            .delay((d, i) => i * delayBetweenCirclesInMs)
             .attr('r', circleRadius)
             .attr('opacity', '1')
+            .on('end', function () {
+                introAnimationCompleted = true
+            })
+
+        svg.selectAll('circle')
+            .on('mouseover', function () {
+                if (!introAnimationCompleted) {
+                    return
+                }
+                d3.select(this)
+                    .transition()
+                    .duration(500)
+                    .attrTween('fill', function () {
+                        const startColor = d3.rgb(circleColor)
+                        const endColor = d3.rgb(circleColorOnHover)
+                        return function (t) {
+                            return d3.interpolate(startColor, endColor)(t)
+                        }
+                    })
+            })
+            .on('mouseout', function () {
+                if (!introAnimationCompleted) {
+                    return
+                }
+                d3.select(this)
+                    .transition()
+                    .duration(500)
+                    .attrTween('fill', function () {
+                        const startColor = d3.rgb(circleColorOnHover)
+                        const endColor = d3.rgb(circleColor)
+                        return function (t) {
+                            return d3.interpolate(startColor, endColor)(t)
+                        }
+                    })
+            })
     }
 
     render() {
