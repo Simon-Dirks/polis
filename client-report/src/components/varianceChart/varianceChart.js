ac8c433e448fb97a08b40515ddeb31591f68eaf0
Simon <Simon-Dirks@users.noreply.github.com>
2024-02-06 14:37:16 +0100

    Show actual comments in variance chart

--- client-report/src/components/varianceChart/varianceChart.js
+++ client-report/src/components/varianceChart/varianceChart.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import * as d3 from 'd3'
+import DataUtils from '../../util/dataUtils'
 
 const circleColor = '#D9D9D9'
 const circleColorOnHover = '#929292'
@@ -8,12 +9,13 @@ const timeForHoverAnimationInMs = 250
 const timeForIntroAnimationInMs = 750
 const delayBetweenCirclesInMs = 3 // TODO: Adjust this based on number of total circles? Configure total animation time based on this
 
+const minVotesForCommentToShow = 7
+
 class VarianceChart extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            numRows: 4, // Number of rows
-            numCirclesPerRow: 36, // Number of circles per row
+            numCirclesPerRow: 36,
             padding: 3,
             selectedComment: undefined,
             introAnimationCompleted: false,
@@ -100,6 +102,7 @@ class VarianceChart extends Component {
 
     initCircles(svg, circlesData, circleRadius) {
         const padding = this.state.padding - 1 // quick fix for making sure the very far right pixel of a circle is not cut off
+        const svgHeight = svg.node().getBoundingClientRect().height
 
         return svg
             .selectAll('circle')
@@ -107,9 +110,65 @@ class VarianceChart extends Component {
             .enter()
             .append('circle')
             .attr('cx', (d) => (d.index + 0.5) * (circleRadius * 2 + padding))
-            .attr('cy', (d) => (d.row + 0.5) * (circleRadius * 2 + padding))
+            .attr('cy', (d) => svgHeight - (d.row + 0.5) * (circleRadius * 2 + padding))
             .attr('r', circleRadius)
             .attr('fill', '#D9D9D9')
+    }
+
+    getCirclesData() {
+        const circlesData = []
+
+        let comments = this.props.comments
+        if (!comments || comments.length === 0) {
+            return []
+        }
+
+        comments = comments.filter((comment) => comment.saw >= minVotesForCommentToShow)
+
+        let minVariance = Infinity
+        let maxVariance = 0
+        const commentsWithVariance = comments.map((comment) => {
+            const agreedVotes = new Array(comment.agreed).fill(1)
+            const disagreedVotes = new Array(comment.disagreed).fill(-1)
+            const commentVotesForVariance = agreedVotes.concat(disagreedVotes)
+            const variance = DataUtils.calculateVariance(commentVotesForVariance)
+            comment.variance = variance
+            minVariance = variance < minVariance ? variance : minVariance
+            maxVariance = variance > maxVariance ? variance : maxVariance
+            return comment
+        })
+
+        const numSlots = this.state.numCirclesPerRow
+        const slotWidth = (maxVariance - minVariance) / numSlots
+        const slots = new Array(numSlots).fill(null).map(() => [])
+
+        let maxSlotItems = 0
+        commentsWithVariance.forEach((comment) => {
+            const variance = comment.variance
+            if (variance >= minVariance && variance <= maxVariance) {
+                const slotIndex = Math.min(
+                    Math.floor((variance - minVariance) / slotWidth),
+                    numSlots - 1
+                )
+                slots[slotIndex].push(comment)
+                const numSlotItems = slots[slotIndex].length
+
+                maxSlotItems = numSlotItems > maxSlotItems ? numSlotItems : maxSlotItems
+            }
+        })
+
+        for (let slotColIdx = 0; slotColIdx < this.state.numCirclesPerRow; slotColIdx++) {
+            const slotItems = slots[slotColIdx]
+            for (let slotRowIdx = 0; slotRowIdx < slotItems.length; slotRowIdx++) {
+                const slotItem = slotItems[slotRowIdx]
+                circlesData.push({
+                    row: slotRowIdx,
+                    index: slotColIdx,
+                    text: JSON.stringify(slotItem),
+                })
+            }
+        }
+        return [circlesData, maxSlotItems]
     }
 
     createSVG(animate = true) {
@@ -118,29 +177,19 @@ class VarianceChart extends Component {
         const containerWidth = container.clientWidth
         const containerHeight = container.clientHeight
 
+        const [circlesData, numRows] = this.getCirclesData()
+
         const circleRadius = this.calculateRadius(
             containerWidth,
             containerHeight,
             this.state.numCirclesPerRow,
-            this.state.numRows,
+            numRows,
             this.state.padding
         )
 
         svg.selectAll('circle').remove()
 
         svg.attr('width', '100%').attr('height', '100%')
-
-        const circlesData = []
-
-        for (let colIdx = 0; colIdx < this.state.numCirclesPerRow; colIdx++) {
-            for (let rowIdx = this.state.numRows - 1; rowIdx >= 0; rowIdx--) {
-                circlesData.push({
-                    row: rowIdx,
-                    index: colIdx,
-                    text: 'fdkjsalfj' + Math.random().toString(),
-                })
-            }
-        }
 
         const circles = this.initCircles(svg, circlesData, circleRadius)
 
@@ -157,7 +206,7 @@ class VarianceChart extends Component {
                     {this.state.selectedComment && <p>{this.state.selectedComment}</p>}
                 </div>
 
-                <div className="flex-grow">
+                <div className="flex-grow mb-4">
                     <svg ref={this.svgRef} className="w-full h-full"></svg>
                 </div>
             </div>
