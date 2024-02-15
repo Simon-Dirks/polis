77c5d045f90eeb02e748e1b32a8604001f41b179
Simon <Simon-Dirks@users.noreply.github.com>
2024-02-15 11:59:47 +0100

    Update helpButton.js

--- client-report/src/components/helpButton.js
+++ client-report/src/components/helpButton.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Tooltip } from './tooltip'
-import { groupColor, groupLabels } from './globals'
 
 const helpButton = ({ math }) => {
     const getNumberOfGroups = () => {
@@ -26,44 +25,49 @@ const helpButton = ({ math }) => {
                 placement="top"
                 color={'#333740'}
             >
-                {/*TODO: Make help content dynamic*/}
-                <div className={'w-64 sm:w-96 rounded-lg p-6 mr-4 bg-[#333740] text-white'}>
-                    <p className={'font-bold mb-2'}>Waar kijk ik naar?</p>
-                    <p className={'mb-4'}>
-                        Je kijkt nu naar het overzicht van alle deelnemers. Ieder bolletje is iemand
-                        die gestemd heeft en de positie wordt bepaald op basis van stemgedrag.
-                        Deelnemers die eenzelfde stemgedrag hebben, worden met elkaar in een groep
-                        geplaatst en krijgen de kleur van die groep.
+                <div
+                    className={
+                        'w-64 sm:w-96 rounded-lg p-6 mr-4 bg-kennislink-dark-blue text-white'
+                    }
+                >
+                    <p className={'font-semibold text-sm mb-2'}>Waar kijk ik naar?</p>
+                    <p className={'text-sm'}>
+                        Elke cirkel is een stelling. Klik op een stelling om te zien hoe er gestemd
+                        is. Als de meeste mensen hetzelfde hebben gestemd (meerderheid ‘eens’ of
+                        ‘oneens’), is er veel overeenstemming. Die stellingen staan links.
+                        Stellingen waar juist heel verschillend op is gestemd, verschijnen rechts.
+                        Dat zijn stellingen met meer verdeeldheid. Gebruik de knop rechtsboven om
+                        zelf te stemmen.
                     </p>
 
-                    <div className={'grid grid-cols-2'}>
-                        {getGroupIds().map((groupId) => {
-                            return (
-                                <p className={'mb-2'} key={groupId}>
-                                    <svg
-                                        viewBox="0 0 20 20"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className={'h-6 inline-block mr-3'}
-                                        fill={groupColor(groupId)}
-                                    >
-                                        <circle cx="10" cy="10" r="10" />
-                                    </svg>
-                                    <span>Groep {groupLabels[groupId]}</span>
-                                </p>
-                            )
-                        })}
+                    {/*<div className={'grid grid-cols-2'}>*/}
+                    {/*    {getGroupIds().map((groupId) => {*/}
+                    {/*        return (*/}
+                    {/*            <p className={'mb-2'} key={groupId}>*/}
+                    {/*                <svg*/}
+                    {/*                    viewBox="0 0 20 20"*/}
+                    {/*                    xmlns="http://www.w3.org/2000/svg"*/}
+                    {/*                    className={'h-6 inline-block mr-3'}*/}
+                    {/*                    fill={groupColor(groupId)}*/}
+                    {/*                >*/}
+                    {/*                    <circle cx="10" cy="10" r="10" />*/}
+                    {/*                </svg>*/}
+                    {/*                <span>Groep {groupLabels[groupId]}</span>*/}
+                    {/*            </p>*/}
+                    {/*        )*/}
+                    {/*    })}*/}
 
-                        <p className={'mb-2'}>
-                            <svg
-                                viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg"
-                                className={'h-6 inline-block mr-3 fill-[#A8A8A8]'}
-                            >
-                                <circle cx="10" cy="10" r="10" />
-                            </svg>
-                            <span>Geen</span>
-                        </p>
-                    </div>
+                    {/*    <p className={'mb-2'}>*/}
+                    {/*        <svg*/}
+                    {/*            viewBox="0 0 20 20"*/}
+                    {/*            xmlns="http://www.w3.org/2000/svg"*/}
+                    {/*            className={'h-6 inline-block mr-3 fill-[#A8A8A8]'}*/}
+                    {/*        >*/}
+                    {/*            <circle cx="10" cy="10" r="10" />*/}
+                    {/*        </svg>*/}
+                    {/*        <span>Geen</span>*/}
+                    {/*    </p>*/}
+                    {/*</div>*/}
                 </div>
             </Tooltip>
         </div>
