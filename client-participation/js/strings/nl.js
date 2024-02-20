98ae94d2b683f5844f415576ff2c6dbd861a0f1a
Simon <Simon-Dirks@users.noreply.github.com>
2024-02-20 12:43:33 +0100

    Style participation

--- client-participation/js/strings/nl.js
+++ client-participation/js/strings/nl.js
@@ -2,14 +2,15 @@
 
 var s = {};
 
-s.agree = "EENS";
-s.disagree = "ONEENS";
-s.pass = "OVERSLAAN";
+s.agree = "Eens";
+s.disagree = "Oneens";
+s.pass = "Overslaan";
 
 s.modSpam = "Spam";
 s.modOffTopic = "Niet relevant";
 s.modImportant = "Belangrijk";
-s.modSubmitInitialState = "Overslaan (geen enkel van bovenstaande), volgende stelling";
+s.modSubmitInitialState =
+  "Overslaan (geen enkel van bovenstaande), volgende stelling";
 s.modSubmit = "Klaar, ga naar volgende stelling";
 
 s.x_wrote = "schreef:";
@@ -21,19 +22,25 @@ s.comment_123 = "Typerende stellingen:";
 s.majorityOpinion = "Meerderheidsmening";
 s.majorityOpinionShort = "Meerderheid";
 s.info = "Info";
-s.addPolisToYourSite = "<img style='height: 20px. margin: 0px 4px.' src='{{URL}}'/>";
+s.addPolisToYourSite =
+  "<img style='height: 20px. margin: 0px 4px.' src='{{URL}}'/>";
 s.privacy = "Privacy";
 s.TOS = "Servicevoorwaarden";
-s.writePrompt = "Bijvoorbeeld: We moeten de nadruk leggen op genezen in plaats van verbeteren.";
+s.writePrompt =
+  "Bijvoorbeeld: We moeten de nadruk leggen op genezen in plaats van verbeteren.";
 s.anonPerson = "Anoniem";
 s.helpWhatAmISeeingTitle = "Wat ben ik aan het bekijken?";
-s.helpWhatAmISeeing = "Mensen met een gelijkaardige mening zijn gegroepeerd. Klik op een groep om hun mening te bekijken.";
+s.helpWhatAmISeeing =
+  "Mensen met een gelijkaardige mening zijn gegroepeerd. Klik op een groep om hun mening te bekijken.";
 s.helpWhatDoIDoTitle = "Wat moet ik doen?";
-s.helpWhatDoIDo = "Stem op andere stellingen door op 'akkoord' of 'niet akkoord' te klikken. Schrijf een stelling (blijf kort en bondig, één idee per lijn). Nodig vrienden uit om deel te nemen aan de discussie!";
-s.writeCommentHelpText = "<strong>schrijf</strong> een stelling indien uw mening niet wordt vertegenwoordigd</i>";
+s.helpWhatDoIDo =
+  "Stem op andere stellingen door op 'akkoord' of 'niet akkoord' te klikken. Schrijf een stelling (blijf kort en bondig, één idee per lijn). Nodig vrienden uit om deel te nemen aan de discussie!";
+s.writeCommentHelpText =
+  "<strong>schrijf</strong> een stelling indien uw mening niet wordt vertegenwoordigd</i>";
 s.helpWriteListIntro = "Wat is een goed voorstel?";
 s.helpWriteListStandalone = "Een idee dat op zichzelf staat";
-s.helpWriteListRaisNew = "Kaart nieuwe perspectieven, ervaringen of problemen aan";
+s.helpWriteListRaisNew =
+  "Kaart nieuwe perspectieven, ervaringen of problemen aan";
 s.helpWriteListShort = "Duidelijk en beknopt (beperkt tot 140 tekens)";
 s.heresHowGroupVoted = "Groep {{GROUP_NUMBER}} stemde:";
 s.one_person = "{{x}} persoon";
@@ -45,72 +52,103 @@ s.xOfthoseDisagreed = "van deze deelnemers zijn het oneens";
 s.opinionGroups = "Meningengroepen";
 s.pctAgreed = "{{pct}}% van alle deelnemers is het eens";
 s.pctDisagreed = "{{pct}}% van alle deelnemers is het oneens";
-s.pctAgreedLong = "{{pct}}% van alle deelnemers is het eens met stelling {{comment_id}}.";
+s.pctAgreedLong =
+  "{{pct}}% van alle deelnemers is het eens met stelling {{comment_id}}.";
 s.pctAgreedOfGroup = "{{pct}}% van Groep {{group}} is het eens";
 s.pctDisagreedOfGroup = "{{pct}}% van Groep {{group}} is het oneens";
-s.pctDisagreedLong = "{{pct}}% van alle deelnemers is het oneens met stelling {{comment_id}}.";
-s.pctAgreedOfGroupLong = "{{pct}}% van Groep {{group}} is het eens met stelling {{comment_id}}.";
-s.pctDisagreedOfGroupLong = "{{pct}}% van Groep {{group}} is het oneens met stelling {{comment_id}}.";
-s.commentSent = "Stelling verzonden! Andere deelnemers zullen uw stelling zien en daarop stemmen.";
+s.pctDisagreedLong =
+  "{{pct}}% van alle deelnemers is het oneens met stelling {{comment_id}}.";
+s.pctAgreedOfGroupLong =
+  "{{pct}}% van Groep {{group}} is het eens met stelling {{comment_id}}.";
+s.pctDisagreedOfGroupLong =
+  "{{pct}}% van Groep {{group}} is het oneens met stelling {{comment_id}}.";
+s.commentSent =
+  "Stelling verzonden! Andere deelnemers zullen uw stelling zien en daarop stemmen.";
 s.commentSendFailed = "Er was een fout bij het toevoegen van uw stelling.";
-s.commentSendFailedEmpty = "Er was een fout bij het toevoegen van uw stelling - de stelling mag niet leeg zijn.";
-s.commentSendFailedTooLong = "Er was een fout bij het toevoegen van uw stelling - de stelling is te lang.";
-s.commentSendFailedDuplicate = "Er was een fout bij het toevoegen van uw stelling - deze stelling bestaat al.";
+s.commentSendFailedEmpty =
+  "Er was een fout bij het toevoegen van uw stelling - de stelling mag niet leeg zijn.";
+s.commentSendFailedTooLong =
+  "Er was een fout bij het toevoegen van uw stelling - de stelling is te lang.";
+s.commentSendFailedDuplicate =
+  "Er was een fout bij het toevoegen van uw stelling - deze stelling bestaat al.";
 s.commentErrorDuplicate = "Dubbel! Deze stelling bestaat al.";
-s.commentErrorConversationClosed = "Deze conversatie is afgesloten. Reageren is niet meer toegestaan.";
+s.commentErrorConversationClosed =
+  "Deze conversatie is afgesloten. Reageren is niet meer toegestaan.";
 s.commentIsEmpty = "Stelling is leeg";
 s.commentIsTooLong = "Stelling is te lang";
 
 s.connectFacebook = "Verbinden met Facebook";
 s.connectTwitter = "Verbinden met Twitter";
-s.connectToPostPrompt = "Verbindt om te reageren, we zullen niets publiceren op uw tijdlijn.";
-s.connectToVotePrompt = "Verbindt om te stemmen, we zullen niets publiceren op uw tijdlijn.";
+s.connectToPostPrompt =
+  "Verbindt om te reageren, we zullen niets publiceren op uw tijdlijn.";
+s.connectToVotePrompt =
+  "Verbindt om te stemmen, we zullen niets publiceren op uw tijdlijn.";
 s.tip = "Tip:";
 s.commentWritingTipsHintsHeader = "Tips om stellingen te schrijven";
-s.tipCharLimit = "De lengte van stellingen zijn beperkt to {{char_limit}} tekens.";
-s.tipCommentsRandom = "Stellingen zijn willekeurig gesorteerd. U bent op niemand aan het reageren";
-s.tipOneIdea = "Splits lange stellingen op in verschillende ideeën. Dit maakt het makkelijker voor anderen om te stemmen.";
-s.tipNoQuestions = "Probeer vragen in uw stellingen te vermijden. Deelnemers zullen vervolgens al dan niet akkoord gaan met uw stelling.";
-s.commentTooLongByChars = "De lengte van uw stelling werd met  {{CHARACTERS_COUNT}} karakters overschreden.";
+s.tipCharLimit =
+  "De lengte van stellingen zijn beperkt to {{char_limit}} tekens.";
+s.tipCommentsRandom =
+  "Stellingen zijn willekeurig gesorteerd. U bent op niemand aan het reageren";
+s.tipOneIdea =
+  "Splits lange stellingen op in verschillende ideeën. Dit maakt het makkelijker voor anderen om te stemmen.";
+s.tipNoQuestions =
+  "Probeer vragen in uw stellingen te vermijden. Deelnemers zullen vervolgens al dan niet akkoord gaan met uw stelling.";
+s.commentTooLongByChars =
+  "De lengte van uw stelling werd met  {{CHARACTERS_COUNT}} karakters overschreden.";
 s.notSentSinceDemo = "(niet echt, dit is een demo)";
-s.submitComment = "INDIENEN";
+s.submitComment = "Indienen";
 s.tipStarred = "Gemarkeerd als belangrijk.";
-s.participantHelpWelcomeText = "Welkom bij een nieuwe soort discussie - <span style='font-weight: 700.'>stem</span> op de meningen van personen en draag bij tot het gesprek door zelf te reageren.";
-s.participantHelpGroupsText = "Als veel mensen hetzelfde stemmen op bepaalde stellingen, ontstaat een meningengroep. Kijk waar jij staat ten opzichte van andere meningen.";
-s.participantHelpGroupsNotYetText = "De visualisatie zal verschijnen zodra er 7 personen zijn begonnen met stemmen";
-s.helpWhatAreGroupsDetail = "<p>U hebt allicht al 'aanbevolen artikels' op Amazon, of ‘aanbevolen films' op Netflix. Elk van deze services gebruiken statistieken om u te groeperen in bij personen die dezelfde dingen kopen of bekijken, vervolgend wordt er u getoond wat deze mensen kochten of naar welke film ze keken.</p> <p> Wanneer u een stem uitbrengt op een stelling wordt u vervolgens gegroepeerd met mensen die dezelfde stem uitbrachten! Onderaan kan u deze groepen bekijken. Elk van deze groepen bestaan uit personen met dezelfde opinie. Er zijn fascinerende inzichten de ontdekken elk van deze gesprekken. Wacht niet langer – klik op een groep om te zien wat hun bijeen bracht en wat hun uniek maakt! </p>";
-s.socialConnectPrompt = "Verbind optioneel om te zien welke vrienden en andere personen uw volgt in de visualisatie.";
+s.participantHelpWelcomeText =
+  "Welkom bij een nieuwe soort discussie - <span style='font-weight: 700.'>stem</span> op de meningen van personen en draag bij tot het gesprek door zelf te reageren.";
+s.participantHelpGroupsText =
+  "Als veel mensen hetzelfde stemmen op bepaalde stellingen, ontstaat een meningengroep. Kijk waar jij staat ten opzichte van andere meningen.";
+s.participantHelpGroupsNotYetText =
+  "De visualisatie zal verschijnen zodra er 7 personen zijn begonnen met stemmen";
+s.helpWhatAreGroupsDetail =
+  "<p>U hebt allicht al 'aanbevolen artikels' op Amazon, of ‘aanbevolen films' op Netflix. Elk van deze services gebruiken statistieken om u te groeperen in bij personen die dezelfde dingen kopen of bekijken, vervolgend wordt er u getoond wat deze mensen kochten of naar welke film ze keken.</p> <p> Wanneer u een stem uitbrengt op een stelling wordt u vervolgens gegroepeerd met mensen die dezelfde stem uitbrachten! Onderaan kan u deze groepen bekijken. Elk van deze groepen bestaan uit personen met dezelfde opinie. Er zijn fascinerende inzichten de ontdekken elk van deze gesprekken. Wacht niet langer – klik op een groep om te zien wat hun bijeen bracht en wat hun uniek maakt! </p>";
+s.socialConnectPrompt =
+  "Verbind optioneel om te zien welke vrienden en andere personen uw volgt in de visualisatie.";
 s.connectFbButton = "Verbind met Facebook";
 s.connectTwButton = "Verbind met Twitter";
-s.polis_err_reg_fb_verification_email_sent = "Gelieve uw mail te controleren voor een verificatie link, kom vervolgens terug op deze pagina.";
-s.polis_err_reg_fb_verification_noemail_unverified = "Uw Facebook account is niet geverifieerd. Gelieven eerst uw emailadres te controleren bij Facebook, kom vervolgens terug op deze pagina om verder te gaan.";
+s.polis_err_reg_fb_verification_email_sent =
+  "Gelieve uw mail te controleren voor een verificatie link, kom vervolgens terug op deze pagina.";
+s.polis_err_reg_fb_verification_noemail_unverified =
+  "Uw Facebook account is niet geverifieerd. Gelieven eerst uw emailadres te controleren bij Facebook, kom vervolgens terug op deze pagina om verder te gaan.";
 s.showTranslationButton = "Toon vertalingen";
 s.hideTranslationButton = "Verberg vertalingen";
 
-s.notificationsAlreadySubscribed = "U bent al geabonneerd voor updates in dit gesprek.";
-s.notificationsGetNotified = "Wordt op de hoogte gebracht zodra er nieuwe stellingen verschijnen:";
-s.notificationsEnterEmail = "Vul uw emailadres in om op de hoogte gesteld te worden van nieuwe stellingen:";
+s.notificationsAlreadySubscribed =
+  "U bent al geabonneerd voor updates in dit gesprek.";
+s.notificationsGetNotified =
+  "Wordt op de hoogte gebracht zodra er nieuwe stellingen verschijnen:";
+s.notificationsEnterEmail =
+  "Vul uw emailadres in om op de hoogte gesteld te worden van nieuwe stellingen:";
 s.labelEmail = "Email";
 s.notificationsSubscribeButton = "Abonneer";
 s.noCommentsYet = "Er zijn nog geen stellingen beschikbaar.";
-s.noCommentsYetSoWrite = "Start dit gesprek door als eerste een stelling toe te voegen.";
-s.noCommentsYetSoInvite = "Start dit gesprek door nieuwe deelnemers toe te voegen, of voeg als eerste een stelling toe.";
+s.noCommentsYetSoWrite =
+  "Start dit gesprek door als eerste een stelling toe te voegen.";
+s.noCommentsYetSoInvite =
+  "Start dit gesprek door nieuwe deelnemers toe te voegen, of voeg als eerste een stelling toe.";
 s.noCommentsYouVotedOnAll = "U hebt op alle stellingen gestemd.";
-s.noCommentsTryWritingOne = "Als u zelf iets wilt toevoegen aan het gesprek, probeer dan een stelling te schrijven via de knop rechtsboven.";
+s.noCommentsTryWritingOne =
+  "Als u zelf iets wilt toevoegen aan het gesprek, probeer dan een stelling te schrijven via de knop rechtsboven.";
 s.convIsClosed = "Dit gesprek is afgesloten.";
 s.noMoreVotingAllowed = "Er mag niet meer gestemd worden.";
 
 s.topic_good_01 = "Wat zouden we kunnen doen met de pingpong kamer?";
-s.topic_good_01_reason = "vrijblijvend, iedereen kan een opinie of vraag hebben voor deze vraag";
+s.topic_good_01_reason =
+  "vrijblijvend, iedereen kan een opinie of vraag hebben voor deze vraag";
 s.topic_good_02 = "Wat denkt u over het nieuwe voorstel?";
-s.topic_good_02_reason = "vrijblijvend, iedereen kan een opinie of vraag hebben voor deze vraag";
+s.topic_good_02_reason =
+  "vrijblijvend, iedereen kan een opinie of vraag hebben voor deze vraag";
 s.topic_good_03 = "Kan u iets bedenken dat de productiviteit vertraagt?";
 
-s.topic_bad_01 = "iedereen moet rapporteren over hun vorderingen i.v.m. de start";
-s.topic_bad_01_reason = "verschillende mensen van verschillende teams zullen reageren op het antwoord, zij zullen echt niet allemaal de kennis hebben om vastbesloten te kunnen stemmen.";
+s.topic_bad_01 =
+  "iedereen moet rapporteren over hun vorderingen i.v.m. de start";
+s.topic_bad_01_reason =
+  "verschillende mensen van verschillende teams zullen reageren op het antwoord, zij zullen echt niet allemaal de kennis hebben om vastbesloten te kunnen stemmen.";
 s.topic_bad_02 = "welke factoren remmen onze start af?";
 s.topic_bad_02_reason = "";
 
-
 module.exports = s;
-
