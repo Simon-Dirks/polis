c8fc5a0a16c755c4188249d77d2f88faf84abedb
Simon <Simon-Dirks@users.noreply.github.com>
2024-02-27 11:52:39 +0100

    Replace vertical axis text with svgs to prevent cross-browser alignment issues

--- client-report/src/components/varianceChart/verticalVarianceAxis.js
+++ client-report/src/components/varianceChart/verticalVarianceAxis.js
@@ -5,19 +5,16 @@ import arrowDown from '../../assets/arrow-up.svg'
 const VerticalVarianceAxis = ({ height }) => {
     return (
         <div
-            className={'absolute top-0 -left-12 md:hidden text-xl mt-2 w-4'}
+            className={'absolute top-0 -left-12 md:hidden text-xl mt-2 w-6'}
             style={{
                 height: `${height}px`,
-                writingMode: 'vertical-rl',
             }}
         >
-            <p className={'absolute top-0 left-0 rotate-180'}>
-                <span className={'relative left-[6px]'}>Stellingen met overeenstemming</span>
-                <img src={arrowUp} alt={'Arrow icon'} className={'h-8 inline mt-2'} />
+            <p className={'absolute top-0 left-0'}>
+                <img src={arrowDown} alt={'Arrow icon'} className={'mb-2 inline w-6'} />
             </p>
-            <p className={'absolute bottom-0 left-0 pb-10 rotate-180'}>
-                <img src={arrowDown} alt={'Arrow icon'} className={'h-8 mb-2 inline'} />
-                <span className={'relative left-[6px]'}>Stellingen met verdeeldheid</span>
+            <p className={'absolute bottom-0 left-0 pb-10'}>
+                <img src={arrowUp} alt={'Arrow icon'} className={'inline mt-2 w-6'} />
             </p>
         </div>
     )
