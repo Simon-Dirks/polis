3c3a5d523a471c14c1456c499681b50daa7d822d
Simon <Simon-Dirks@users.noreply.github.com>
2024-02-27 14:08:06 +0100

    Reduce font size of welcome overlay on desktop

--- client-report/src/components/welcomeOverlay.js
+++ client-report/src/components/welcomeOverlay.js
@@ -17,8 +17,8 @@ const WelcomeOverlay = ({ math, ptptCountTotal }) => {
 
     return (
         <dialog id="welcome_overlay_modal" className="modal">
-            <div className="modal-box p-8 md:p-12 text-base md:text-2xl bg-kennislink-dark-blue text-white max-w-5xl">
-                <p className={'text-xl md:text-5xl font-medium mb-6'}>
+            <div className="modal-box p-8 md:p-12 text-base md:text-lg bg-kennislink-dark-blue text-white max-w-5xl">
+                <p className={'text-xl md:text-3xl font-medium mb-6'}>
                     Hoe moeten we omgaan met het aanpassen van embryo-DNA?
                 </p>
                 <p className={'mb-4'}>
@@ -51,11 +51,11 @@ const WelcomeOverlay = ({ math, ptptCountTotal }) => {
                     </ul>
                 </div>
 
-                <div className={'mt-8 text-left md:text-center'}>
+                <div className={'mt-8 text-left'}>
                     <form method="dialog" className={'inline-block'}>
                         <button
                             className={
-                                'btn btn-outline text-white rounded-full border border-white hover:border-white hover:bg-white hover:text-kennislink-dark-blue font-normal text-base md:text-xl block md:inline-block mb-2 md:mb-0'
+                                'btn btn-outline text-white rounded-full border border-white hover:border-white hover:bg-white hover:text-kennislink-dark-blue font-normal text-base md:text-lg block md:inline-block mb-2 md:mb-0'
                             }
                         >
                             Ontdek nu
@@ -65,7 +65,7 @@ const WelcomeOverlay = ({ math, ptptCountTotal }) => {
                     <span className={'hidden md:inline-block mx-4'}> of </span>
                     <button
                         className={
-                            'btn btn-outline text-white rounded-full border border-white hover:border-white hover:bg-white hover:text-kennislink-dark-blue font-normal text-base md:text-xl block md:inline-block'
+                            'btn btn-outline text-white rounded-full border border-white hover:border-white hover:bg-white hover:text-kennislink-dark-blue font-normal text-base md:text-lg block md:inline-block'
                         }
                         onClick={() => {
                             closeModalButtonRef.current?.click()
