74503deccf4206810ab58efa789f6137431dcacc
Simon <Simon-Dirks@users.noreply.github.com>
2024-02-28 11:31:57 +0100

    Add select-none to some elems and update selection color

--- client-report/src/components/varianceChart/horizontalVarianceAxis.js
+++ client-report/src/components/varianceChart/horizontalVarianceAxis.js
@@ -9,12 +9,16 @@ const HorizontalVarianceAxis = ({ width }) => {
             style={{ width: `${width}px` }}
         >
             <p className={'absolute top-0 left-0'}>
-                <img src={arrowLeft} alt={'Arrow icon'} className={'h-8 mr-2 inline'} />
+                <img src={arrowLeft} alt={'Arrow icon'} className={'h-8 mr-2 inline select-none'} />
                 <span>Stellingen met overeenstemming</span>
             </p>
             <p className={'absolute top-0 right-8'}>
                 <span>Stellingen met verdeeldheid</span>
-                <img src={arrowRight} alt={'Arrow icon'} className={'h-8 ml-2 inline'} />
+                <img
+                    src={arrowRight}
+                    alt={'Arrow icon'}
+                    className={'h-8 ml-2 inline select-none'}
+                />
             </p>
         </div>
     )
